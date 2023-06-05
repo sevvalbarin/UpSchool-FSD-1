62503e955beb57f87e0d87cd12daa766b47e2068
Şevval Barın <sevvalbarn07@gmail.com>
2023-06-05 19:29:24 +0300

    Homework1 fixed

---
+++ Ödev 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace PasswordGenerator
+{
+    public class Program
+    {
+        
+        static int Main(string[] args)
+        {
+            var passwordGenerator = new PasswordsGenerator();
+
+            passwordGenerator.Greetings();
+
+            passwordGenerator.ReadInputs();
+
+            passwordGenerator.Generate();
+
+            passwordGenerator.WriteLatestGeneratedPassword();
+
+            Console.ReadLine();
+
+            return 0;
+        }
+    }
+}
